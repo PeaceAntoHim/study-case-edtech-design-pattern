7a6da9272944b84a297388eb2036b0ea00f02c61
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2024-05-06 01:27:30 +0700

    feat: sigin

--- pages/api/user/signin/signin.handler.ts
+++ pages/api/user/signin/signin.handler.ts
@@ -2,11 +2,13 @@ import { query } from "@/dependency-injection/dependency-injection";
 
 export default async function handle(
   req: { method?: string; body?: any },
-  res: { status: (arg0: number) => any; setHeader: (arg1: any) => any }
+  res: { status: (arg0: number) => any; setHeader: (arg1: any, arg2: any) => any }
 ) {
   if (req.method === "POST") {
     const { email, password } = req.body;
-    query.login(email, password);
+    const payload = await query.login(email, password);
+    res.setHeader("Token", payload);
+    return res.status(200).json(payload);
   } else {
     return res.status(405).json({ message: "Method Not allowed" });
   }
